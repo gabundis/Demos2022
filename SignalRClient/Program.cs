1d2da1994f524d092a72b95e4443560e3354d796
Gilberto Abundis <gabundis@bnext.mx>
2022-12-26 12:59:25 -0600

    Se agrega proyecto SignalRClient para probar conexión a proyecto SignalR

---
+++ SignalRClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+
+namespace SignalRClient
+{
+    internal class Program 
+    {
+        static async Task Main()
+        {
+            using (HubConnection hubConnection = new HubConnection("http://notificationservices1.southcentralus.cloudapp.azure.com"))
+            {
+                IHubProxy messageHubProxy = hubConnection.CreateHubProxy("MessageHub");
+                await hubConnection.Start();
+
+                Console.WriteLine(hubConnection.State.ToString());
+                Console.WriteLine("Press Enter key to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
