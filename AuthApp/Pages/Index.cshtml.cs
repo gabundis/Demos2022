761f53bf74f5643ab864d32e0bc5b898f289de91
Gilberto Abundis <gabundis@bnext.mx>
2023-03-13 15:31:13 -0600

    AuthApp and KeyApp

---
+++ AuthApp/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Identity.Web;
+
+namespace AuthApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly ITokenAcquisition _tokenAcquisition;
+        public string accessToken;
+        public string blobContent;
+
+        public IndexModel(ILogger<IndexModel> logger, ITokenAcquisition tokenAcquisition)
+        {
+            _logger = logger;
+            _tokenAcquisition = tokenAcquisition;
+        }
+
+        [Obsolete]
+        public async Task OnGet()
+        {
+            TokenAcquisitionTokenCredential credential = new(_tokenAcquisition);
+
+            Uri blobUri = new("https://gabundisaz204storage.blob.core.windows.net/data/script.sql");
+            BlobClient blobClient = new(blobUri, credential);
+
+            MemoryStream ms = new();
+            blobClient.DownloadTo(ms);
+            ms.Position = 0;
+
+            StreamReader sr = new(ms);
+            blobContent = sr.ReadToEnd();
+        }
+    }
+}
---
+++ Keyapp/Program.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
+using System.Text;
+
+string tenantId = "";
+string clientId = "";
+string clientSecret = "";
+
+ClientSecretCredential clientSecretCredential = new(tenantId, clientId, clientSecret);
+
+string keyVaultUrl = "https://gsam-az204-keyvault.vault.azure.net/";
+string keyName = "appkey";
+string textToEncrypt = "This is a secret text";
+
+KeyClient keyClient = new(new Uri(keyVaultUrl), clientSecretCredential);
+
+var key = keyClient.GetKey(keyName);
+
+CryptographyClient cryptographyClient = new(key.Value.Id, clientSecretCredential);
+
+byte[] textToBytes = Encoding.UTF8.GetBytes(textToEncrypt);
+
+EncryptResult result = cryptographyClient.Encrypt(EncryptionAlgorithm.RsaOaep, textToBytes);
+
+Console.WriteLine("The encrypted string");
+Console.WriteLine(Convert.ToBase64String(result.Ciphertext));
+
+byte[] cipherToBytes = result.Ciphertext;
+
+DecryptResult textDecrypted = cryptographyClient.Decrypt(EncryptionAlgorithm.RsaOaep, cipherToBytes);
+
+Console.WriteLine(Encoding.UTF8.GetString(textDecrypted.Plaintext));
+
+Console.ReadKey();
